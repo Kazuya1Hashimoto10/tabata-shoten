b644d89fb11add1e973ca686eb678f8fe1637cc1
Kazuya1Hashimoto10 <kazuya110gtw@icloud.com>
2020-03-04 15:48:45 +0900

    eventのindexを作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root "top#index"
 
   resources :blogs, only: [:index]
+  resources :events, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
